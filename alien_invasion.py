7521c73419dd83b0eff7b7525bdf4a2bbce14a5f
Nathan Brooks <nathangamer0806@gmail.com>
2026-04-21 10:18:35 -0400

    Score updates even if multiple aliens are hit at once

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,6 +85,7 @@ class AlienInvasion:
 
             # Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
 
             self.game_active = True
 
@@ -162,7 +163,8 @@ class AlienInvasion:
                 self.bullets, self.aliens, True, True)
         
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
         
         if not self.aliens:
